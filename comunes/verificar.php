647502a37b8b0e8c2ed35f2b66b1eed405fb481e
Brouse13 <unaiunai2004@gmail.com>
2020-04-28 11:18:45 +0200

    Added 'my reports' tu users & added MC code

---
+++ comunes/verificar.php
@@ -0,0 +1,27 @@
+<?php
+    function generarCodigo($longitud) {//Genera un código aleatorio depeniendo de la lungitud dada como parámetro
+        $key = '';
+        $pattern = '1234567890abcdefghijklmnopqrstuvwxyz';
+        $max = strlen($pattern)-1;
+        for($i=0;$i < $longitud;$i++) $key .= $pattern{mt_rand(0,$max)};
+        return $key;
+       }
+
+
+    $sql = 'SELECT * FROM users WHERE Username="'.$_SESSION['User'].'"';
+    if($result = $con->query($sql)){
+        while($row = $result->fetch_array()){
+            if($row['Uuid']==NULL){//El user no está registrado
+                if($row['Verification_Code']==NULL){//Primera vez que entra el user
+                    $Code = generarCodigo(5);
+                    if($stm = $con->query('UPDATE users SET Verification_Code="'.$Code.'" WHERE Username="'.$_SESSION['User'].'"')){
+                        echo '<div class="alert alert-danger alert-dismissible fade show"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                        Por favor, usa el código '.$code.' para poder hacer reportes</div>';
+                    }
+                }
+                echo '<div class="alert alert-danger alert-dismissible fade show"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                Por favor, usa el código '.$row['Verification_Code'].' para poder hacer reportes</div>';
+            }
+        }
+    }
+?>
--- user/index.php
+++ user/index.php
@@ -1,14 +1,93 @@
+<?php
+require('../session.php');
+require('../db.php');
+
+if($stm = $con->prepare('SELECT Rango FROM users WHERE Username=?')){//Método para obtener rango del propietario de la sesión
+	$stm->bind_param('s',$_SESSION['User']);
+	$stm->execute();
+	$stm->store_result();
+	$stm->bind_result($Rango);
+	$stm->fetch();
+}
+$count=0;
+if($stm = $con->prepare('SELECT * FROM reportes WHERE Reportante=?')){//Método para obtener rango del propietario de la sesión
+	$stm->bind_param('s',$_SESSION['User']);
+	$stm->execute();
+	$stm->store_result();
+	$count= $stm->num_rows();
+}
+?>
 <!DOCTYPE html>
 <html>
 <?php include '../comunes/header.php'; ?>
 <body>
 <div class="container">
+<?php include '../comunes/verificar.php';  //Función para generar código de verificación y/o caonfirmar si el user está registrado?>
+<nav class="navbar navbar-expand-xl bg-dark navbar-dark">
+	<ul class="navbar-nav mr-auto">
+		<li class="nav-item dropdown active">
+		  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown"><?php echo 'Hola '.$_SESSION['User'];?></a>
+		  <div class="dropdown-menu">
+			<a class="dropdown-item" href="#">Cuenta</a>
+			<a class="dropdown-item" href="#">Cambiar contraseña</a>
+			<a class="dropdown-item" href="../logout.php">Salir</a>
+		  </div>
+		</li>
+		<li class="nav-item active">
+		  <a class="nav-link" href="index.php">Inicio</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="reportes.php">Reportar</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="apelaciones.php">Apelalar</a>
+		</li>
+	</ul>
+</nav>
+	<hr>
 	<div class="row">
 		<div class="col-sm-12">
-			<h2 class="text-center" style="color:orange">User</h2>
+			<h3 style="color:orange"><?php echo 'Hola de nuevo '.$Rango.' '.$_SESSION['User'];?></h3><!--Hola de nuevo $Rango $Nombre-->
 		</div>
 	</div>
 	<hr>
+	<!-- Inicio del body-->
+
+	<div class="row"><!--Número total de reportes-->
+		<h4 class="col">Tus reportes: <?php echo $count; ?></h4>
+	</div>
+
+	<div class="mt-sm-4"><!--Lista de todos tus reportes con query de sql-->
+		<?php
+		$sql = 'SELECT * FROM reportes WHERE Reportante="'.$_SESSION['User'].'"';
+		if($result = $con->query($sql)){
+			if($result->num_rows>0){
+				echo'<table id="reportes_table" class="table table-striped table-hover">
+					<thead class="thead-dark">
+					<tr>
+						<th>Reportado</th>
+						<th>Motivo</th>
+						<th>Resuelto Por</th>
+						<th>Comentarios</th>	
+					</tr>
+					</thead>
+					<tbody id="reportes">';
+				while($row = $result->fetch_array()){
+					if($row['Estado']=='resuelto'){$EstadoDisplay='Resuelto';$ColorRow='table-success';}else if($row['Estado']=='rechazado'){$EstadoDisplay='Rechazado';$ColorRow='table-danger';}
+						else{$EstadoDisplay='Esperando';$ColorRow='table-warning';}//Colores de las tablas según el estado del reporte
+						
+						echo '<tr id="'.$row['Id'].'" class="'.$ColorRow.'">';
+						echo '<td>'.$row['Reportado'].'</td>';
+						echo '<td>'.$row['Motivo'].'</td>';
+						echo '<td>'.$row['Resueltopor'].'</td>';
+						echo '<td><textarea name="comentarios_staff" class="form-control col-sm-11" rows="3" readonly>'.$row['Comentarios_user'].'</textarea></td>';
+						echo '</tr>';
+				}
+			}
+		}?>
+		</tbody>
+		</table>
+	</div>
 </div>
 </body>
 </html>
